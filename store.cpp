71d615d29f97d580bea87c5f7d4c0b2a714da883
Jarod Guerrero <jarodgu@uw.edu>
2020-11-08 07:01:31 -0800

    Update store.cpp

--- store.cpp
+++ store.cpp
@@ -1,5 +1,5 @@
 // ------------- store.cpp -------------------------------------------------
-// Jarod Guerrero, Ajeet Dhaliwal, Kaib Cropley CSS 343 C
+// Jarod Guerrero, Ajeet Dhaliwal, Kaib Cropley CSS ThreeFourThree C
 // Created 3/9/19
 // Last Modified 3/20/19
 // -------------------------------------------------------------------------
